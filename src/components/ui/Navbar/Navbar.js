195a3e44dc046b2e7a897b3622e3957046385d50
Pavel Ray <pray@falabella.cl>
2023-02-21 11:00:11 +0530

    Mobile style updated

--- src/components/ui/Navbar/Navbar.js
+++ src/components/ui/Navbar/Navbar.js
@@ -8,25 +8,26 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavbarMenu = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="warning" expand="lg" className="fixed-top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>Aaltapori</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <Nav className="ms-auto">
+            <Link className="nav-link" href="/">
+              Home
+            </Link>
+            <NavDropdown title="Gallary" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+                <Link href="/gallary">Photo Gallary</Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item>
+                <Link href="/gallary">Video Gallary</Link>
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link>
+              <Link href="/contact"> Contact</Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -34,4 +35,4 @@ const NavbarMenu = () => {
   );
 };
 
-export default Navbar;
+export default NavbarMenu;
